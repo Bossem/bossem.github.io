91da0785c2f30b45cc0af44c091cda456feebd46
Роман Бойченко <60850154+Bossem@users.noreply.github.com>
2020-03-09 15:11:56 +0500

    изминил отступы

--- DISIGN-REPAIR/gulpfile.js
+++ DISIGN-REPAIR/gulpfile.js
@@ -1,6 +1,27 @@
 const {src,dest,watch}=require('gulp')
 const browserSync=require('browser-sync').create();
 const sass=require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer');
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+    return gulp.src('styles/*.css')
+      .pipe(cleanCSS({debug: true}, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      }))
+    .pipe(gulp.dest('dist'));
+  });
+
+
+function ga() {
+  return gulp.src('./css')
+    .pipe(autoprefixer({
+      browsers: ['last 7.0.1 versions']
+    }))
+    .pipe(gulp.dest('css'));
+};
 
 // Static server
 function bs() {
