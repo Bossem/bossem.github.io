024c01ee2730fa4394819f320dcfdd536d65b56f
Роман Бойченко <60850154+Bossem@users.noreply.github.com>
2020-03-09 22:29:10 +0500

    переписал код

--- DISIGN-REPAIR/gulpfile.js
+++ DISIGN-REPAIR/gulpfile.js
@@ -6,12 +6,12 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-css', () => {
-    return gulp.src('styles/*.css')
-      .pipe(cleanCSS({debug: true}, (details) => {
-        console.log(`${details.name}: ${details.stats.originalSize}`);
-        console.log(`${details.name}: ${details.stats.minifiedSize}`);
-      }))
-    .pipe(gulp.dest('dist'));
+  return gulp.src('styles/*.css')
+  .pipe(cleanCSS({debug: true}, (details) => {
+  console.log(`${details.name}: ${details.stats.originalSize}`);
+  console.log(`${details.name}: ${details.stats.minifiedSize}`);
+  }))
+  .pipe(gulp.dest('dist'));
   });
 
 
