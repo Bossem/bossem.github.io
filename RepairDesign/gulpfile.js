5a02927c80886d4f51104fa80c579ef4b6775338
Роман Бойченко <60850154+Bossem@users.noreply.github.com>
2020-03-14 11:58:55 +0500

    Добавил Слайдер

--- RepairDesign/gulpfile.js
+++ RepairDesign/gulpfile.js
@@ -1,33 +1,17 @@
 // const gulp = require('gulp');
-// const browserSync = require('browser-sync').create();
-// const cleanCSS = require('gulp-clean-css');
-// const rename = require("gulp-rename");
-
-// // Static server
-// gulp.task('browser-sync', function() {
-//   browserSync.init({
-//       server: {
-//           baseDir: "./"
-//       }
-//   });
-//   gulp.watch("./*.html").on('change', browserSync.reload);
-// });
-
-// // Minify and rename CSS files
-// gulp.task('mincss', function() {
-//   return gulp.src("./css/*.css")  
-//   .pipe(rename({suffix: ".min"}))  
-//   .pipe(cleanCSS())  
-//   .pipe(gulp.dest("./css"));  
-// });
-
-
-const {src, dest, watch} = require('gulp');
+const { src, dest, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
+
+
+const fileCss = src(['./css/*.css', '!./css/*.min.css']);
+const fileMinCss = src('./css/*.min.css');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const del = require('del');
+
+
 
 // Static server
 function bs() {
@@ -36,7 +20,7 @@ function bs() {
     server: {
       baseDir: "./"
     },
-    browser: 'google chrome',
+    browser: '',
     notify: false
   });
   watch("./*.html").on('change', browserSync.reload);
@@ -46,27 +30,27 @@ function bs() {
   watch("./js/*.js").on('change', browserSync.reload);
 }
 
-
-
-
-
-// Sass compile in CSS
+// sass
 function serveSass() {
-  return src('./sass/**/*.sass', './src/sass/**/*.scss')
-      .pipe(sass())
-      .pipe(autoprefixer({
-        cascade: false
-      }))
-      .pipe(dest('./**/css'))
-      .pipe(browserSync.stream());
-};
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
+}
 
-// Minify and rename CSS files
-function minCSS() {
-  return src("./css/*.css")  
-  .pipe(rename({suffix: ".min"}))  
-  .pipe(cleanCSS())  
-  .pipe(dest("./css"));  
-};
+// min-css 
+function minCss(done) {
+  fileCss.pipe(rename({ suffix: ".min" })).pipe(cleanCss()).pipe(dest("./css/min"));
+  fileMinCss.pipe(dest("./css/min"));
+  del(['./css/*.min.css']);
+
+  done();
+}
 
 exports.serve = bs;
+exports.minCss = minCss;
+
+
