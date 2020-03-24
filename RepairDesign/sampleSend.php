3166c6a8a17598235a63c2d716512c0074869ae1
Роман Бойченко <60850154+Bossem@users.noreply.github.com>
2020-03-24 18:43:16 +0500

    dist

---
+++ RepairDesign/sampleSend.php
@@ -0,0 +1,49 @@
+<?php
+
+$sampleUserName = $_POST['sampleUserName'];
+$sampleUserEmail = $_POST['sampleUserEmail'];
+$sampleUserPhone = $_POST['sampleUserPhone'];
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->CharSet = "UTF-8";
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                             // Enable verbose debug output
+    $mail->isSMTP();                                  // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                  // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                         // Enable SMTP authentication
+    $mail->Username   = 'rbojcenko2018@gmail.com';          // SMTP username
+    $mail->Password   = '49&k&Rna';                   // SMTP password
+    $mail->SMTPSecure = 'ssl';                        // Enable TLS encryption;
+    $mail->Port       = 465;                          // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('rbojcenko2018@gmail.com');
+    $mail->addAddress('prockopiv.roman2015@yandex.ru');      // Add a recipient
+    
+    // Content
+    $mail->isHTML(true);                              // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    // $mail->Subject = 'New request from web site';
+    $mail->Body    = "Имя пользователя: ${sampleUserName}, телефон: ${sampleUserPhone}. Email пользователя: ${sampleUserEmail}";
+
+    
+    if ($mail->send()) {
+      echo "ok";
+    } else {
+      echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+    
+    // $mail->send();
+    // header('Location: thanks.html');
+    
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
